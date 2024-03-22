ef6473462ba820eba2c5c93587b76a71c939c39d
ji0 <buti12@naver.com>
2024-03-22 16:19:17 +0900

    무한 배너

--- 001. HTML학습/07.ShoppingMall_PJ/js/main1.js
+++ 001. HTML학습/07.ShoppingMall_PJ/js/main1.js
@@ -55,6 +55,78 @@ addEvt(window,"DOMContentLoaded", loadFn);
 ******************************************/
 function loadFn() {
     console.log("로딩완료!");
+    
+    // 이동버튼 대상 : .abtn
+    const abtn = qsa('.abtn');
+    // 변경대상 : #slide
+    const slide = qs('#slide');
+    // console.log(abtn,slide);
 
+    // 왼쪾 버튼 처음에 숨기기
+    abtn[0].style.display = 'none';
+    
+    // 슬라이드 순번 전역변수
+    let snum=0;
+
+    // 버튼 모두 이벤트 설정하기
+    for(let x of abtn){
+        x.onclick=goSlide;
+    }///for of문///
+
+    
+    // // 2. 오른쪽 버튼 클릭시 기능구현
+    // abtn[1].onclick = ()=>{
+
+    // };
+    // // 3. 왼쪽 버튼 클릭시 기능구현
+    // abtn[0].onclick = ()=>{
+   
+    // };
+
+
+    /*********************************************** 
+     * 함수명 : goSlide
+     * 기능 : 슬라이드 이동
+     ***********************************************/
+    function goSlide(){
+        // 두번째 버튼인 .ab2인가?
+        let isRbtn = this.classList.contains('ab2');
+        // [classList객체의 contains) 메서드]
+        // -> 해당요소의 특정 클래스인지 여부를 리턴함 
+        // 해당클래스가 있으면 true, 없으면 false
+
+        // 함수호출 확인
+        console.log('슬라이드 호출', this,isRbtn);
+
+
+        // 오른쪽 버튼일 경우
+        if(isRbtn){
+            slide.style.left = '-100%';
+            slide.style.transition = '.6s ease-in-out';
+            slide.style.transition = '.6s ease-in-out';
+            // 이동하는 시간 0.6초 기다림
+            setTimeout(() => {
+                // 맨 앞 li 맨뒤로 이동
+                slide.appendChild(
+                    slide.querySelectorAll('li')[0]
+                );
+                // 슬라이드 left값이 -100%이므로 left값을 0으로 변경
+                slide.style.left = '0';
+                slide.style.transition = 'none';
+            }, 600);
+
+            // 맨 앞li 맨뒤로 이동하기
+            // appendChild(요소)
+            // -> 원래 뒤에 요소 추가기능임
+            // -> 기존있는 요소를 선택 시 맨 뒤로 이동함
+            
+
+        }//if문//
+
+
+
+    }//////////goSlide함수//////////
+    ///////////////////////////////
+    
 } //////////////// loadFn 함수 ///////////////
 /////////////////////////////////////////////
