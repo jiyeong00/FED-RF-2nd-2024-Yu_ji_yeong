856b1b6006dd49849a318f19fdaa209731a8ade5
ji0 <buti12@naver.com>
2024-04-04 16:10:51 +0900

    익명함수는 화살표함수로 줄여써도 오케이임

--- 003.JS학습/03.JS모듈/module/msg_format.js
+++ 003.JS학습/03.JS모듈/module/msg_format.js
@@ -26,12 +26,22 @@
 // }/////////makeMessage함수//////////
 
 // 3. 익명 함수를 만들고 함수 앞에서 export default하기
-//  ->> 내보내기할때 함수명이 중요치 않으므로 익명함수로 내보내기를 해도 무관하다.
+//  ->> 내보내기할때 함수명이 중요치 않으므로 익명함수로 내보내기를 해도 무관하다
 // 내용 메시지를 구성하는 함수
-export default function (name,age){
-    return `
+// export default function (name,age){
+//     return `
+//         나의 이름은 ${name}입니다.
+//         나이는 ${age}살 입니다.
+//         반갑습니다.^^<br><br>
+//     `;
+// }/////////makeMessage함수//////////
+
+// >>>>>>>>>>> 3번 줄여쓰면
+// 화살표 함수로 내보내기를 해도 무관함.
+// 이때 return 키워드 생략이 가능할 경우 이를 생략한다.
+export default (name,age)=>
+     `
         나의 이름은 ${name}입니다.
         나이는 ${age}살 입니다.
         반갑습니다.^^<br><br>
     `;
-}/////////makeMessage함수//////////
