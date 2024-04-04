570b57dcbac12d4289b8aae8a81a1a43b6adcf84
ji0 <buti12@naver.com>
2024-04-04 16:05:22 +0900

    export default하기
    - 선언적 함수를 만들고 함수 앞에서 export default하기
    
    - 익명 함수를 만들고 함수 앞에서 export default하기

--- 003.JS학습/03.JS모듈/module/msg_format.js
+++ 003.JS학습/03.JS모듈/module/msg_format.js
@@ -1,14 +1,37 @@
 // 구체적인 데이터 구성처리를 위한 JS
 
 // 내용 메시지를 구성하는 함수
-function makeMessage(name,age){
+// function makeMessage(name,age){
+    //     return `
+    //         나의 이름은 ${name}입니다.
+    //         나이는 ${age}살 입니다.
+    //         반갑습니다.^^<br><br>
+    //     `;
+// }/////////makeMessage함수//////////
+
+
+// 내보내기
+// export default makeMessage;
+
+
+// 2. 선언적 함수를 만들고 함수 앞에서 export default하기
+//  ->> 선언적함수 형태가 그대로 유지되어야함. >> 이름은 중요하지 않음
+// 내용 메시지를 구성하는 함수
+// export default function makeMessage(name,age){
+//     return `
+//         나의 이름은 ${name}입니다.
+//         나이는 ${age}살 입니다.
+//         반갑습니다.^^<br><br>
+//     `;
+// }/////////makeMessage함수//////////
+
+// 3. 익명 함수를 만들고 함수 앞에서 export default하기
+//  ->> 내보내기할때 함수명이 중요치 않으므로 익명함수로 내보내기를 해도 무관하다.
+// 내용 메시지를 구성하는 함수
+export default function (name,age){
     return `
         나의 이름은 ${name}입니다.
         나이는 ${age}살 입니다.
         반갑습니다.^^<br><br>
     `;
 }/////////makeMessage함수//////////
-
-
-// 내보내기
-export default makeMessage;
