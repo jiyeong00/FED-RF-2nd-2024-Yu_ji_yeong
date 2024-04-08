2f6fbea4e4a40667da36721f3089c99855df5b7b
ji0 <buti12@naver.com>
2024-04-08 15:12:40 +0900

    ㅇㄴㅁㅇㄴㅁㅇㅁㄴㅇㅁㅇ

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -4,7 +4,7 @@
 import myFn from "./my_function.js";
 
 // 부드러운 스크롤 불러오기
-import { startSS,setScrollPos } from "./smoothScroll23.js";
+import { startSS, setScrollPos } from "./smoothScroll23.js";
 
 // 모듈로 호출된 JS에서는 다른 외부JS를 import로 호출가능!
 // import하려는 파일에서 반드시 함수,변수 등을 export해야함.
@@ -27,56 +27,92 @@ slideFn();
 
 // 3. Intro동영상 파트 클릭 시 동영상태그 넣기
 // 이벤트 대상 = 변경 대상 : .intro-mv-img
-const introMv=myFn.qs('.intro-mv-img');
+const introMv = myFn.qs(".intro-mv-img");
 
-introMv.onclick = ()=>{
-    console.log('인트로 영상');
-    // 1. 동영상 넣기
-    introMv.innerHTML = `
+introMv.onclick = () => {
+  console.log("인트로 영상");
+  // 1. 동영상 넣기
+  introMv.innerHTML = `
     <video src="./images/intro_mv.mp4" autoplay="" controls=""></video>
     `;
-    // 2. 클래스 off 지우기 (플레이 버튼 안나오게)
-    introMv.classList.remove('off');
-};////////////click이벤트
+  // 2. 클래스 off 지우기 (플레이 버튼 안나오게)
+  introMv.classList.remove("off");
+}; ////////////click이벤트
 
-// 2. 미리보기 파트 내용 넣기
-// 로딩시 바로 실행됨 -> 실행코드를 지역화하고자 할때 함수로 만들고 이를 호출하면 됨. <-불편한 방법
-// >>> 익명함수로 만들고 바로 실행하게 하면됨
-// 방법 : (익명함수)()->바로 실햄됨
-// 실제코드 : (()=>{코드})
-// 실제코드 : (function()=>{코드})
-// >>>이러한 처리방법을 '코드의 지역화' 또느 '코드랩핑'이라고 부르기도 함.
-//  -> 목적 : 변수,함수 충돌방지
+/***************************************** 
+ * [ 코드랩핑이란 ]
+ 로딩시 바로 실행됨 -> 실행코드를 지역화하고자 할때 함수로 만들고 이를 호출하면 됨. <-불편한 방법
+
+ >>> 익명함수로 만들고 바로 실행하게 하면됨
+ 방법 : (익명함수)()->바로 실햄됨
+ 실제코드 : (()=>{코드})
+ 실제코드 : (function()=>{코드})
+ >>>이러한 처리방법을 '코드의 지역화' 또느 '코드랩핑'이라고 부르기도 함.
+  -> 목적 : 변수,함수 충돌방지
+  *****************************************/
+
+//  2. 미리보기 파트 내용 넣기
 
 // 미리보기 구현 코드 랩핑구역
-(()=>{
-
-
-// 대상 : preview-box
-const previewBox=myFn.qs('.preview-box');
-// 데이터 : dkb_data.js 의 prewviewData배열
-const pData=dkbData.previewData
-// 구조 : ul>li>h3+p
-// 8개만 데이터를 구성하여 넣는다.
-// html코드 변수
-let hcode=`<ul class="fx-box">`;
-// li구성을 hcode변수에 대입연산자로 할당
-for(let i=0; i<8; i++) {
-    hcode+=`
+(() => {
+  // 대상 : preview-box
+  const previewBox = myFn.qs(".preview-box");
+  // 데이터 : dkb_data.js 의 prewviewData배열
+  const pData = dkbData.previewData;
+  // 구조 : ul>li>h3+p
+  // 8개만 데이터를 구성하여 넣는다.
+  // html코드 변수
+  let hcode = `<ul class="fx-box">`;
+  // li구성을 hcode변수에 대입연산자로 할당
+  for (let i = 0; i < 8; i++) {
+    hcode += `
     <li>
         <h3>${pData[i].title}</h3>
         <p>${pData[i].story}</p>
     </li>
     `;
-}///for문///
-hcode+=`</ul>`;
+  } ///for문///
+  hcode += `</ul>`;
 
-// 데이터 확인하기
-console.log(hcode);
-// console.log('미리보기 data',pData);
+  // 데이터 확인하기
+  console.log(hcode);
+  // console.log('미리보기 data',pData);
 
-// 2. 화면출력하기
-previewBox.innerHTML=hcode;
+  // 2. 화면출력하기
+  previewBox.innerHTML = hcode;
+})(); ////////코드 랩핑구역
 
+//  3. 현장포토 파트 내용 넣기
 
-})();////////코드 랩핑구역
+// 현장포토 구현 코드 랩핑구역
+(() => {
+  // 대상 : live-box
+  const liveBox = myFn.qs(".live-box");
+  // 데이터 : dkb_data.js 의 prewviewData배열
+  const lvData = dkbData.liveData;
+  // 구조 : ul>li>figure>img+figcaption
+  // 8개만 데이터를 구성하여 넣는다.
+  // html코드 변수
+  let hcode = `<ul>`;
+  // li구성을 hcode변수에 대입연산자로 할당
+  //liveData 배열은 총 8개임, 모두 돌기를 세팅하자
+  lvData.forEach(v=>{
+      hcode += `
+      <li>
+          <figure>
+              <img src="./images/live_photo/${v.imgName}.jpg" alt="${v.title}">
+              <figcaption>${v.title}</figcaption>
+          </figure>
+      </li>
+      `;
+  });
+
+  hcode += `</ul>`;
+
+  // 데이터 확인하기
+  console.log(hcode);
+  console.log('미리보기 data',lvData);
+
+  // 2. 화면출력하기
+  liveBox.innerHTML = hcode;
+})(); ////////코드 랩핑구역
