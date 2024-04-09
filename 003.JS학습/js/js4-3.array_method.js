09882db5e8a99bc66a1e383343ce3d4395053037
ji0 <buti12@naver.com>
2024-04-09 16:12:11 +0900

    Update js4-3.array_method.js
    (7)중간배열삽입 버튼 : splice()
    fruit.splice(anum.value,0,sel.value);

--- 003.JS학습/js/js4-3.array_method.js
+++ 003.JS학습/js/js4-3.array_method.js
@@ -92,15 +92,15 @@ sel.innerHTML = newArr.join("");
 // 대상 : #anum < anum변수
 // 데이터 : fruit 배열
 // 갱신시 계속 재바인딩 되어야 함! (함수화 필요!)
-const bindCombo=()=>{
-    anum.innerHTML = fruit
-      .map(
-        (v, i) => `
+const bindCombo = () => {
+  anum.innerHTML = fruit
+    .map(
+      (v, i) => `
         <option value="${i}">${v}</option>
     `
-      )
-      .join("");
-};/////////////bindCombo함수//////////////
+    )
+    .join("");
+}; /////////////bindCombo함수//////////////
 
 // 콤보박스 데이터 바인딩 함수 최초호출!
 bindCombo();
@@ -160,28 +160,42 @@ function showFruit() {
     fruit.shift();
   }
   // (6) 중간배열삭제 버튼 : splice()
-  // 삭제 시 : splice(순번) -> 해당순번부터 뒤를 모두 삭제 
+  // 삭제 시 : splice(순번) -> 해당순번부터 뒤를 모두 삭제
   //           splice(순번,개수) -> 해당순번부터 개수만큼 삭제
-  else if (btxt ===  "중간배열삭제") {
+  else if (btxt === "중간배열삭제") {
     // 지울 순번읽어오기(대상 :#anum)
-    let delSeq=anum.value;
+    let delSeq = anum.value;
     // 지울 개수 읽어오기(대상:#delnum);
-    let delCnt=delnum.value;
+    let delCnt = delnum.value;
 
-    console.log('삭제할 시작순번:',delSeq,'\n지울 개수:',delCnt);
+    console.log("삭제할 시작순번:", delSeq, "\n지울 개수:", delCnt);
 
     // 입력할 지울개수가 숫자가 아니면 1로 넣기
     // isNaN(변수) -> 숫자가 아니면 true, 맞으면 false
-    if(isNaN(delCnt)){
-        // 변수값 1로 변경
-        delCnt=1;
-        // 입력창에도 1로 넣기
-        delnum.value=1;
+    if (isNaN(delCnt)) {
+      // 변수값 1로 변경
+      delCnt = 1;
+      // 입력창에도 1로 넣기
+      delnum.value = 1;
     }
+
     // 대상 : fruit 배열
-    fruit.splice(delSeq,delCnt);
+    fruit.splice(delSeq, delCnt);
     // splice(지울순번,지울개수)
   }
+
+//   (7)중간배열삽입 버튼 : splice()
+// ((삽입시))
+// splice(순번,0,넣을값,넣을값,.....)
+// -> 순번뒤에 전달값 0을 쓰고 그 뒤에 넣을 값을 나열
+// -> 결과 : 선택순번 앞쪽에 배열값이 삽입됨
+else if(btxt==='중간배열삽입'){
+    // 대상 : fruit
+    // fruit.splice(순번,0,넣을값)
+    fruit.splice(anum.value,0,sel.value);
+
+    console.log('넣을순번앞에 삽입:', anum.value);
+}
   ///////공통실행코드구역/////////
 
   // 처음배열출력함수
