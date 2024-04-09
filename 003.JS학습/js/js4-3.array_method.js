be72c2d25cf2f6aa3d28f27c1b35966fa9e6ad8f
ji0 <buti12@naver.com>
2024-04-09 16:03:34 +0900

    Update js4-3.array_method.js
    isNaN(변수) -> 숫자가 아니면 true, 맞으면 false
    
    중간배열삭제 버튼 : splice()
    splice(지울순번,지울개수)
    >>    fruit.splice(delSeq,delCnt);

--- 003.JS학습/js/js4-3.array_method.js
+++ 003.JS학습/js/js4-3.array_method.js
@@ -92,13 +92,18 @@ sel.innerHTML = newArr.join("");
 // 대상 : #anum < anum변수
 // 데이터 : fruit 배열
 // 갱신시 계속 재바인딩 되어야 함! (함수화 필요!)
-anum.innerHTML = fruit
-  .map(
-    (v, i) => `
-    <option value="${i}">${v}</option>
-`
-  )
-  .join("");
+const bindCombo=()=>{
+    anum.innerHTML = fruit
+      .map(
+        (v, i) => `
+        <option value="${i}">${v}</option>
+    `
+      )
+      .join("");
+};/////////////bindCombo함수//////////////
+
+// 콤보박스 데이터 바인딩 함수 최초호출!
+bindCombo();
 
 // 4. 이벤트 설정하기
 mbtn.forEach((ele) => {
@@ -154,10 +159,36 @@ function showFruit() {
     // 대상 : fruit 배열
     fruit.shift();
   }
+  // (6) 중간배열삭제 버튼 : splice()
+  // 삭제 시 : splice(순번) -> 해당순번부터 뒤를 모두 삭제 
+  //           splice(순번,개수) -> 해당순번부터 개수만큼 삭제
+  else if (btxt ===  "중간배열삭제") {
+    // 지울 순번읽어오기(대상 :#anum)
+    let delSeq=anum.value;
+    // 지울 개수 읽어오기(대상:#delnum);
+    let delCnt=delnum.value;
+
+    console.log('삭제할 시작순번:',delSeq,'\n지울 개수:',delCnt);
+
+    // 입력할 지울개수가 숫자가 아니면 1로 넣기
+    // isNaN(변수) -> 숫자가 아니면 true, 맞으면 false
+    if(isNaN(delCnt)){
+        // 변수값 1로 변경
+        delCnt=1;
+        // 입력창에도 1로 넣기
+        delnum.value=1;
+    }
+    // 대상 : fruit 배열
+    fruit.splice(delSeq,delCnt);
+    // splice(지울순번,지울개수)
+  }
   ///////공통실행코드구역/////////
 
   // 처음배열출력함수
   showArray();
+
+  // 콤보박스 바인딩 함수 호출!
+  bindCombo();
 
   // fruit배열확인
   console.log("fruit배열 : ", fruit);
