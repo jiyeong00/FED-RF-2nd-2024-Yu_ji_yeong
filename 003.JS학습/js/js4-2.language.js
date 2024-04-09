0f7dfdb5edb425afbe816664401c35e0c21ad23b
ji0 <buti12@naver.com>
2024-04-09 11:01:29 +0900

    asd

--- 003.JS학습/js/js4-2.language.js
+++ 003.JS학습/js/js4-2.language.js
@@ -2,4 +2,8 @@
 
 // 나의 함수 불러오기
 import mFn from './my_function.js';
-console.log(mFn);
+// console.log(mFn);
+
+// 제이슨 데이터 불러오기 : 어서써 타입 제이슨 / 같이써 타입 제이슨
+import langData from './data_lang.json' with{type:"json"};
+console.log(langData);
