b2a3566d960dfc2abe7b0db656207ef25bcba2ec
ji0 <buti12@naver.com>
2024-04-29 11:53:49 +0900

    ㄹㅇㅎ

--- 003.JS학습/01.JS실험실/js/dance.js
+++ 003.JS학습/01.JS실험실/js/dance.js
@@ -9,13 +9,13 @@ const slidePg = mFn.qs(".slidePg");
 
 // 2. 코드변수에 태그 만들어 넣기
 let hcode = "<ul>";
-for(let i=1; i<=7;i++){
-    hcode+=`
+for (let i = 1; i <= 7; i++) {
+  hcode += `
     <li>
     <img src="./images/dance/${i}.jpg" alt="댄스 이미지">
     </li>
     `;
-}////for문//////
+} ////for문//////
 
 hcode += "</ul>";
 
@@ -29,30 +29,57 @@ slidePg.innerHTML = hcode;
 // 대상 : window
 // 이벤트 종류 : scroll
 // 위치 기준 대상 : .tpg -> 스티키를 싸고 있는 부모박스
-const tpg=mFn.qs('.tpg');
+const tpg = mFn.qs(".tpg");
 // 움직을 대상 :.slidePg>ul
-const target =mFn.qs('.slidePg>ul');
+const target = mFn.qs(".slidePg>ul");
+// 추가 대상 : .slidePg>ul>li
+const eachList = mFn.qsaEl(target, "li");
 
 // 2. 이벤트 설정하기
-mFn.addEvt(window,'scroll',moveSlide);
+mFn.addEvt(window, "scroll", moveSlide);
 
 // 3. 함수 만들기
-function moveSlide(){
-    // 1. 스티키 부모박스 바운딩 top값
-    let bTop=mFn.getBCR(tpg);
-    console.log('바운딩 탑값: ',bTop);
+// (1) 슬라이드 이동함수
+function moveSlide() {
+  // 1. 스티키 부모박스 바운딩 top값
+  let bTop = mFn.getBCR(tpg);
+  // console.log('바운딩 탑값: ',bTop);
 
-    // 2. 이동할 타켓박스 left값으로 부모 바운딩 top값 넣기
-    // (1) 바운딩 top값이 0초과일때 처음위치 재설정
-    if(bTop>0){
-        target.style.left = '0px';
-    }
-    // (1) 바운딩 top값이 0이하/ -3000이상 일때 부모바운딩 top값으로 위치 이동하기
-    else if(bTop<=0 && bTop>=-3000){
-        target.style.left = bTop+'px';
-    }
-    // (3) 마지막 한계 이후엔 한계값으로 세팅
-    else{
-        target.style.left = '-3000px';
-    }
-}//////////////moveSlide함수////////////////////////
+  // 2. 이동할 타켓박스 left값으로 부모 바운딩 top값 넣기
+  // (1) 바운딩 top값이 0초과일때 처음위치 재설정
+  if (bTop > 0) {
+    target.style.left = "0px";
+  }
+  // (1) 바운딩 top값이 0이하/ -3000이상 일때 부모바운딩 top값으로 위치 이동하기
+  else if (bTop <= 0 && bTop >= -3000) {
+    target.style.left = bTop + "px";
+  }
+  // (3) 마지막 한계 이후엔 한계값으로 세팅
+  else {
+    target.style.left = "-3000px";
+  }
+
+  // 3. 개별리스트 상하이동함수 호출하기
+  eachList.forEach((ele, idx) => {
+    upDownFn(ele, idx);
+  });
+} //////////////moveSlide함수////////////////////////
+
+// (2) 리스트 상하 이동함수
+function upDownFn(ele, idx) {
+  //순번
+
+  //스티키박스 하위 li가 기준화면 left선에 대한 위치값 바운딩left값을 이용하여 자기 위치에서 위/아래로 이동하는 수치변경하기
+  let mVal = mFn.getBCR2(ele);
+  // 위치값을 양수로 만들고 윈도우 크기로 나눈 후 백분율 (수치가 크므로 2로 나누어줌)
+  mVal = (Math.abs(mVal / window.innerWidth) * 100) / 2;
+  console.log(mVal);
+
+  //   3. 들어온 li순번이 짝수일 경우 마이너스 처리
+  //   짝수일 경우 마이너스를 곱하여 방향을 반대로 해준다
+  // 짝수는 %연산자 사용 : 숫자%2=0 >>짝수
+  if (idx % 2 == 0) mVal = -mVal;
+
+  //   실제 위치이동 CSS적용하기
+  ele.style.translate = `0 ${mVal}%`;
+} //////////////upDownFn///////////////////
