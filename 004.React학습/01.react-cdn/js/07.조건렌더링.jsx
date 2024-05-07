3e93f16b5d9859dd3303ea77598f80eb98765f95
ji0 <buti12@naver.com>
2024-05-07 15:23:10 +0900

    ㅇㅇ

--- 004.React학습/01.react-cdn/js/07.조건렌더링.jsx
+++ 004.React학습/01.react-cdn/js/07.조건렌더링.jsx
@@ -4,7 +4,7 @@
 import mFn from "./my_function";
 
 // 출력대상 : .root
-const root = mFn.qsa('.root');
+const root = mFn.qsa(".root");
 
 /**************************************** 
     1. if문을 사용하여 조건부 렌더링하기
@@ -12,4 +12,70 @@ const root = mFn.qsa('.root');
     렌더링 할 수 있다!
 ****************************************/
 
+// 1번 컴포넌트
+function MakeDeveloper() {
+  return <h1>나는 개발자야</h1>;
+} /////////////MakeDeveloper 컴포넌트///////////////
 
+// 2번 컴포넌트
+function LostDeveloper() {
+  return <h1>개발자가 뭐지</h1>;
+} /////////////LostDeveloper 컴포넌트///////////////
+
+// 3번 컴포넌트
+function MakeImage({ isrc, ialt, itit }) {
+  return <img src={isrc} alt={ialt} title={itit} />;
+} /////////////MakeImage 컴포넌트///////////////
+
+// 컴포넌트 내부에서 호출하는 다른 컴포넌트를 보통 "서브컴포넌트"라고 부른다.
+// 메인 출력 컴포넌트
+function Developer({ isNormal, isrc, ialt, itit }) {
+  // isNormal은 개발자가 아니면 true
+  if (isNormal) {
+    return (
+      <React.Fragment>
+        <LostDeveloper />
+        <MakeImage isrc={isrc} ialt={ialt} itit={itit} />
+      </React.Fragment>
+    ); //return////
+  } /////if문/////
+
+  //   위에 if문 return에 걸리면 아래쪽 return에는 내려오지 않는다
+  return (
+    <React.Fragment>
+      <MakeDeveloper />
+      <MakeImage isrc={isrc} ialt={ialt} itit={itit} />
+    </React.Fragment>
+  ); //return////
+} /////////////Developer 컴포넌트/////////////
+
+// 이미지경로 배열
+const devImg = [
+  "https://cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/HYAONH6EGJBKIU5CJWWF343MKE.jpg",
+  "https://img3.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202208/24/BoiledMovie/20220824133926904mopw.png",
+];
+
+// 컴포넌트 호출하기 1 : 개발자 찍기
+// 먼저 가상돔에 컴포넌트 리턴코드를 넣어준다.
+ReactDOM.render(
+  <Developer
+    isNormal={false}
+    isrc={devImg[0]}
+    ialt="개발자 공유"
+    itit="프론트앤드 개발자입니다"
+  />,
+  root[0]
+);
+// 컴포넌트 호출하기 2 : 개발자 찍기
+// 먼저 가상돔에 컴포넌트 리턴코드를 넣어준다.
+ReactDOM.render(
+  <Developer
+    isNormal={true}
+    isrc={devImg[1]}
+    ialt="개발자 공유"
+    itit="프론트앤드 개발자입니다"
+  />,
+  root[1]
+);
+// ReactDOM.render(어쩌구,저쩌구);
+// 어쩌구를 저쩌구에 출력해라
