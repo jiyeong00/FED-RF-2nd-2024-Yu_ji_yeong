f9d9e006dde1a6c918270500f1e7f7deb01481fe
ji0 <buti12@naver.com>
2024-05-07 16:11:11 +0900

    리액트만 가능한 true/false 조건문
    리턴 내에서
    {wList.length > 0 && (출력내용)}

--- 004.React학습/01.react-cdn/js/07.조건렌더링.jsx
+++ 004.React학습/01.react-cdn/js/07.조건렌더링.jsx
@@ -79,3 +79,61 @@ ReactDOM.render(
 );
 // ReactDOM.render(어쩌구,저쩌구);
 // 어쩌구를 저쩌구에 출력해라
+
+/********************************************** 
+    2. if문이 아닌 조건 표현하기
+    -> 조건식 && JSX표현식
+    조건이 true일때만 && 뒤의 JSX표현식이 출력됨!
+**********************************************/
+// 개발자 취향 알아보기
+// 2-1. 제목을 찍기 위한 타이틀 컴포넌트
+function SetTitle({ title }) {
+  return <h1>💻개발자💻가 좋아하는 {title}</h1>;
+} ///////////////SetTitle컴포넌트
+
+// 음식리스트
+const foods = ["스파게티", "짜파게티", "냉면", "짜장면", "마라탕"];
+
+// 2-2. 반복리스트를 위한 컴포넌트
+function FoodList({ foodName }) {
+  return <li>개발자는 {foodName} 좋아해 </li>;
+} ////////FoodList 컴포넌트//////////
+
+// 2-3. 개발자 선호 음식 리스트 출력 컴포넌트
+function WishList({ wList }) {
+  //wList - 좋아하는 음식리스트
+
+  return (
+    <React.Fragment>
+      {/* 음식리스트 타이틀 */}
+      <SetTitle title="음식" />
+      {wList.length > 0 && (
+        <div>
+          <h2>개발자가 좋아하는 음식은 모두 {wList.length}가지입니다.</h2>
+          <ul>
+            {
+              wList.map((v) => (
+                <FoodList foodName={v} />
+              ))
+              // 배열변수.map() 메서드 사용
+              // map메서드는 원래 새로운 배열을 현재 자리에 출력하는 용도임
+              // 그러나 리액트는 이것을 변경하여 표현식 안에서 출력 시 태그JSX 형식으로 변환해줌
+              // JS처럼 map().join()처리 불필요
+            }
+          </ul>
+        </div>
+      )}
+      {/* 배열값이 0인 경우 다른 것 출력하기 */}
+      {
+         wList.length == 0 && (
+              <h2> 아직 개발자 음식리스트가 업데이트 되지 않았습니다.</h2>
+          )
+      }
+    </React.Fragment>
+  );
+} ////////WishList 컴포넌트///////////////
+
+// 컴포넌트 출력하기 : 배열값 있는 경우
+ReactDOM.render(<WishList wList={foods} />, root[2]);
+// 컴포넌트 출력하기 : 배열값 없는 경우
+ReactDOM.render(<WishList wList={[]} />, root[3]);
