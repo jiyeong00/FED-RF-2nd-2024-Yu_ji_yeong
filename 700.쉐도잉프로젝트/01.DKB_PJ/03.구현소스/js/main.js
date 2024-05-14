39d37e8833f0dbc2a92a999af676496a8fab3853
ji0 <buti12@naver.com>
2024-05-14 17:09:54 +0900

    배열순회 메서드 비교
    .show()는 diplay를 보여주는 메서드
    .hide()는 display를 숨기는 메서드
    .toggle()는 display를 토글하는 메서드
    
    find()는 조건에 맞을때 return true하면 해당 배열값이 변수에 할당된다.
    forEach()는 모두 순회한다
    
    J쿼리 닫기버튼 이벤트 설정하기
    $(".cbtn").click(()=>subContBox.hide());

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -20,23 +20,18 @@ import setSlide from "./drag_slide_multi.js";
 // 서브박스 세팅 코드 불러오기
 import showSubBox from "./sub_cont.js";
 // 박스 생성 후 호출 (큐로 보내면 스택 실행 후 호출<< 맨 끝에 코드 쓸 필요 없음)
-setTimeout(showSubBox,0);
+setTimeout(showSubBox, 0);
 // 시간을 0으로 쓰거나 시간을 안써도 setTimeout로 함수를 호출하면 큐에 가지고 있다가 스택이 다 끝난후 호출하여 실행
 
 ///////////////////////////////////////////////
 
 /// 구현코드 파트 //////////////
 
-
-
 // 1. 부드러운 스크롤 호출
-const mySmooth = new SmoothScroll(document,30,20);
-
+const mySmooth = new SmoothScroll(document, 30, 20);
 
 // console.log('모듈로 메인JS호출!!!',
 // document.querySelector('.top-menu'));
-
-
 
 // 3. 인트로 동영상 파트 클릭시 동영상태그 넣기
 // 이벤트 대상 === 변경대상 : .intro-mv-img
@@ -88,26 +83,39 @@ introMv.onclick = () => {
 
   // 1. 8개만 데이터를 html로 구성하여 넣는다!
   // html 코드변수
-  let hcode = `<ul class="fx-box">`;
+  // let hcode = `<ul class="fx-box">`;
 
   // li구성을 hcode변수에 대입연산자로 할당함!
-  for (let i = 0; i < 8; i++) {
-    hcode += `
-        <li>
-            <h3>${pData[i].title}</h3>
-            <p>${pData[i].story}</p>
-        </li>
-    `;
-  } //// for //////
+  // for (let i = 0; i < 8; i++) {
+  //   hcode += `
+  //       <li>
+  //           <h3>${pData[i].title}</h3>
+  //           <p>${pData[i].story}</p>
+  //       </li>
+  //   `;
+  // } //// for //////
 
-  hcode += `</ul>`;
+  // hcode += `</ul>`;
 
   // 데이터 확인
   // console.log(hcode);
   // console.log('대상:',previewBox,'미리보기 data:',pData);
 
-  // 2. 화면출력하기 ///////
-  previewBox.innerHTML = hcode;
+  // 2. 화면출력하기 -> map()으로 한번에 출력하기 ///////
+  previewBox.innerHTML = `
+  <ul class="fx-box">
+  ${pData
+    // data-idx - 데이터 키
+    .map(
+      (v) => `
+      <li data-idx="${v.idx}">
+        <h3>${v.title}</h3>
+        <p>${"방송일 : "+v.date+"<br>"+v.story}</p>
+      </li>
+    `
+    ).join("")}
+  </ul>
+  `;
 })(); //// 미리보기 코드랩핑구역 종료 /////////
 
 // 3. 현장포토 파트 내용 넣기 //////////
@@ -184,16 +192,16 @@ introMv.onclick = () => {
 
 // 5. 최신동영상 파트 데이터 태그 구성하여 화면출력하기 ///
 // 코드랩핑구역 시작 //////////////////////////
-(()=>{
+(() => {
   // 5-1. 변경대상: .clip-box
-  const clipBox = myFn.qs('.clip-box');
+  const clipBox = myFn.qs(".clip-box");
 
   // 5-2. 생성코드 변수
   let hcode = `<ul class="slide">`;
 
   // 데이터만큼 순회하여 li코드 만들기 ///
   // 데이터: dkbData.clipData
-  dkbData.clipData.forEach(v=>{
+  dkbData.clipData.forEach((v) => {
     hcode += `
     <li>
       <div class="clip-mv-box">
@@ -206,69 +214,76 @@ introMv.onclick = () => {
       <h3>${v.title}</h3>
     </li>
     `;
-
   }); /////////// forEach /////////////////
 
   hcode += `</ul>`;
 
   // 5-3. 화면출력하기 ///////
   clipBox.innerHTML = hcode;
-
-
 })();
 // 코드랩핑구역 종료 //////////////////////////
 
 // 드래그 슬라이드 태그 구성후 호출하기!
-setSlide('banbx');
-
+setSlide("banbx");
 
 /************************************************* 
   메인 페이지용 도깨비 메뉴 스크롤이동 제이쿼리 구현
  *************************************************/
 // 메뉴 클릭 대상 : .spart-menu a
-$(".spart-menu a").click(e=>{
+$(".spart-menu a").click((e) => {
   // a요소 클릭 시 기본이동 막기
   e.preventDefault();
 
   // 1. 클릭한 a요소의 글자읽어오기
-  let txt=$(e.target).text();
+  let txt = $(e.target).text();
   // console.log(txt);
 
   // 2. 이동할 위치값 알아내기
   let pos;
   // 이동할 위치에 박스 아이디 매칭하기
-  switch(txt){
-    case "프로그램 소개":pos="#intro-area";break;
-    case "미리보기":pos="#preview-area";break;
-    case "동영상":pos="#clip-video-area";break;
-    case "현장 포토":pos="#real-photo-area";break;
-    case "대표 포스터":pos="#main-photo-area";break;
-  }///////switch
+  switch (txt) {
+    case "프로그램 소개":
+      pos = "#intro-area";
+      break;
+    case "미리보기":
+      pos = "#preview-area";
+      break;
+    case "동영상":
+      pos = "#clip-video-area";
+      break;
+    case "현장 포토":
+      pos = "#real-photo-area";
+      break;
+    case "대표 포스터":
+      pos = "#main-photo-area";
+      break;
+  } ///////switch
 
   // 만약 해당된 요소가 없으면 돌아가기
   // 위에서 할당안되면 undefined이면 if문에서 false처리됨
   // !(NOT)연산자로 반대로 뒤집으면 false일때 처리함
-  if(!pos) return;
+  if (!pos) return;
 
   // 해당박스 아이디의 위치값 알아내기
   // offset().top - 제이쿼리 top 위치값정보
-  pos=$(pos).offset().top;
+  pos = $(pos).offset().top;
   // console.log("위치값: ",pos);
 
   // 스크롤 애니메이션 이동하기
   // $("html,body").animate({scrollTop:몇 px},시간, 이징, 함수)
-  $("html,body")
-  .animate({scrollTop:pos+"px"},800,"easeInOutQuint",
-  // 콜백함수
-  ()=>{
-    // 이동후 부드러운 스크롤 위치값 업데이트 필수
-    // 이거 안하면 스크롤 시 튐
-    // 생성자함수 하위 객체 변수로 등록된 함수를 호출함
-    mySmooth.setScrollPos(pos);
-  }
+  $("html,body").animate(
+    { scrollTop: pos + "px" },
+    800,
+    "easeInOutQuint",
+    // 콜백함수
+    () => {
+      // 이동후 부드러운 스크롤 위치값 업데이트 필수
+      // 이거 안하면 스크롤 시 튐
+      // 생성자함수 하위 객체 변수로 등록된 함수를 호출함
+      mySmooth.setScrollPos(pos);
+    }
   );
-
-});/////////도꺠비 파트 메뉴 클릭 함수
+}); /////////도꺠비 파트 메뉴 클릭 함수
 
 ///////////////////////////////////////////////////////////////////
 // 개별박스에 부드러운 스크롤 생성자 함수 적용하기 연습
--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/sub_cont.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/sub_cont.js
@@ -1,20 +1,72 @@
 // 메인 페이지에서 서브 컨텐츠 띄우는 구현코드
+// 데이터 셋팅 불러오기 //////
+import * as dkbData from "../data/dkb_data.js";
 
-export default function showSubBox(){
-    console.log("서브 보임");
+export default function showSubBox() {
+  console.log("서브 보임");
 
-    // 1. 서브 컨첸츠 보이기 기능 구현
-    // (1) 대상선정
-    // 1-1. 이벤트 대상 : 
-    // .preview-box li - 미리보기
-    // .live-box li - 현장포토
-    const subViewBox=$(".preview-box li,.live-box li");
+  // 1. 서브 컨첸츠 보이기 기능 구현
+  // (1) 대상선정
+  // 1-1. 이벤트 대상 :
+  // .preview-box li - 미리보기
+  // .live-box li - 현장포토
+  // .poster-box li- 대표이미지
+  // .clip-box li - 최신동영상
+  const subViewBox = $(`
+    .preview-box li,
+    .live-box li,
+    .poster-box li,
+    .clip-box li
+    `);
 
-    // 1-2. 변경대상 .sub-cont
-    const subContBox=$(".sub-cont");
+  // 1-2. 변경대상 .sub-cont
+  const subContBox = $(".sub-cont");
 
-    // 2. 이벤트 설정 및 함수 구현하기
-    subViewBox.click(function(){
-        console.log("나야나",this);
-    });/////////click
-}///////////////////showSubBox
+  // 2. 이벤트 설정 및 함수 구현하기
+  subViewBox.click(function () {
+    let confPrt = $(this).parent().parent().is(".preview-box");
+    // 위를 JS문법에서 사용하면 아래와 같음
+    // this.parentElement.parentElement.classList.contains(클래스명)
+    console.log("나야나", this,confPrt);
+    // parent()는 바로 위 상위요소로 이동
+
+    if(confPrt){
+        // 1. 키속성값 읽어오기
+        let idx = $(this).attr("data-idx");
+        // attr(속성명) - 속성값 읽어오기 메서드
+        // attr(속성명,속성값) - 속성값 넣기 메서드
+        // console.log("속성값 읽어오기",idx,dkbData.previewData);
+
+        // [ 배열순회 메서드 비교 ]
+        // forEach()는 모두 순회한다
+        // find()는 조건에 맞을때 return true하면 해당 배열값이 변수에 할당된다.
+        //  만약 일치하는 데이터가 없으면 undefined된다.
+        // dkbData.previewData.forEach(v=>{
+        let selData=dkbData.previewData.find(v=>{
+            if(v.idx==idx){
+                // console.log("찾았다",v);
+                return true;
+            }
+            console.log("돌아");
+        });
+
+        console.log("검색결과 : ",selData);
+
+        // 서브박스에 내용 넣기
+        subContBox.html(`
+        <button class="cbtn">×</button>
+        <div class="sub-inbox inbox">
+            <h1>${selData.title}</h1>
+            <div class="sub-item">${selData.story}</div>
+        </div>
+        `).show();
+        // .show()는 diplay를 보여주는 메서드
+        // .hide()는 display를 숨기는 메서드
+        // .toggle()는 display를 토글하는 메서드
+    }//////////if
+
+    // 닫기버튼 이벤트 설정하기
+    $(".cbtn").click(()=>subContBox.hide());
+
+  }); /////////click
+} ///////////////////showSubBox
