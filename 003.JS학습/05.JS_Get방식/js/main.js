5b4c662a8258290c3e557ab7fb1ddb571c8aa570
ji0 <buti12@naver.com>
2024-05-27 11:22:01 +0900

    get방식

--- 003.JS학습/05.JS_Get방식/js/main.js
+++ 003.JS학습/05.JS_Get방식/js/main.js
@@ -1,4 +1,6 @@
 // JS 페이지간 데이터 전달하기 : 메인페이지 JS - main.js
+// 내 함수 가져오기
+import mFn from "./my_function.js";
 
 /********************************************************* 
 [ 웹 페이지간 데이터 전달방식 ]
@@ -28,24 +30,22 @@ Post방식은 데이터 크기에 제한이 없다!)
 // 서브 샵페이지를 호출하기 위한 GNB링크 셋팅하기!
 // 호출시 Get02.html?키=값 형태로 호출함!
 // 1. 대상선정 : #gnb a
-
+const gnb = mFn.qsa("#gnb a");
 
 // 2. 이벤트 설정
-
+gnb.forEach((ele) => mFn.addEvt(ele, "click", goSub));
 
 // 3. 함수만들기
-function goSub(){
-    // 1. 샵명 글자읽기
-    
+function goSub() {
+  // 1. 샵명 글자읽기
+  let atxt = this.innerText;
+  console.log("샵명", atxt);
 
-    // 2. 서브 페이지로 이동하기
-    // location.href = 페이지URL
-    // 현재 브라우저창에서 URL이동함!
-    
-
+  // 2. 서브 페이지로 이동하기
+  // location.href = 페이지URL
+  // 현재 브라우저창에서 URL이동함!
+  location.href = "Get02.html?shop=" + encodeURIComponent(atxt);
 } ///////// goSub 함수 ////////////
-
-
 
 /********************************************************* 
     [ url로 보낼때 한글깨짐 방지하기! ]
