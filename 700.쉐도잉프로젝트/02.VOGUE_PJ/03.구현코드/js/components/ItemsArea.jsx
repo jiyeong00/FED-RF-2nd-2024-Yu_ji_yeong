d2b92dbe7694d98e43a6acebf301c7653011c66a
ji0 <buti12@naver.com>
2024-05-27 13:23:49 +0900

    서브페이지 넘어가기 -리액트

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/ItemsArea.jsx
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/components/ItemsArea.jsx
@@ -9,6 +9,30 @@ export default function ItemsArea({ catName }) {
   const selData = catData[catName];
   console.log(selData);
 
+  // 태그처리 구분코드 생성함수
+  const makeCode=(data)=>{
+    console.log(Array.isArray(data));
+    // 배열 데이터는 태그 구성이 다름
+    // runway 카테고리만 다름
+    if(Array.isArray(data)){
+      return(
+        <h2>
+          <small>
+            {data[0]}
+          </small>
+          <br/>{data[1]}
+        </h2>
+      );
+    }/////if
+    // 배열이 아닌경우
+    else{
+      return(
+        <h2>{data}</h2>
+      );
+    }
+  };//////////////////////makeCode////////////////////
+
+
   // 코드 리턴구역 /////
   return (
     <div id="main-area">
@@ -19,41 +43,44 @@ export default function ItemsArea({ catName }) {
           <h2 className="cat-tit">{selData.제목}</h2>
           {/* <!-- 2-1-2. 서브메뉴(LNB:Local Navigation Bar) --> */}
 
-          {//메뉴가 없음이 아닐때만 배열돌아 출력함
-          selData.메뉴 != "없음" && (
-            <nav className="lnb">
-              <ul>
-                {selData.메뉴.map((v) => (
-                  <li>
-                    <a href="#">{v}</a>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          )}
+          {
+            //메뉴가 없음이 아닐때만 배열돌아 출력함
+            selData.메뉴 != "없음" && (
+              <nav className="lnb">
+                <ul>
+                  {selData.메뉴.map((v) => (
+                    <li>
+                      <a href="#">{v}</a>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            )
+          }
         </header>
         {/* <!-- 2-2. 카테고리 페이지 컨텐츠영역 --> */}
         <div className="cat-cont-area">
+          {/* <!-- 데이터적용4 : 컨텐츠 타이틀 넣기--> */}
           <section className="pt2">
             <div className="cbx bgi bg1-1">
-              <h2></h2>
+              <h2>{makeCode(selData.타이틀[0])}</h2>
             </div>
             <div className="cbx bgi bg1-2">
-              <h2></h2>
+              <h2>{makeCode(selData.타이틀[1])}</h2>
             </div>
             <div className="cbx bgi bg1-3">
-              <h2></h2>
+              <h2>{makeCode(selData.타이틀[2])}</h2>
             </div>
           </section>
           <section className="pt2">
             <div className="cbx bgi bg2-1">
-              <h2></h2>
+              <h2>{makeCode(selData.타이틀[3])}</h2>
             </div>
             <div className="cbx bgi bg2-2">
-              <h2></h2>
+              <h2>{makeCode(selData.타이틀[4])}</h2>
             </div>
             <div className="cbx bgi bg2-3">
-              <h2></h2>
+              <h2>{makeCode(selData.타이틀[5])}</h2>
             </div>
           </section>
         </div>
--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
@@ -3,15 +3,20 @@
 import TopArea from "./components/TopArea";
 import MainArea from "./components/MainArea";
 import FooterArea from "./components/FooterArea";
+// 아이템영역 불러오기
+import ItemsArea from "./components/ItemsArea";
 
 // [1] 메인 페이지 전체 레이아웃 로딩 컴포넌트 ///
 function Layout() {
+  // 상태관리변수 설정구역
+  // [1] 메뉴변경 상태변수
+  const [menu, setMenu] = React.useState("living");
   return (
     <React.Fragment>
       {/* // 1. 상단영역 컴포넌트 */}
-      <TopArea />
+      <TopArea changeMenu={setMenu}/>
       {/* // 2. 메인영역 컴포넌트 */}
-      <MainArea />
+      {menu == "home" ? <MainArea /> : <ItemsArea catName={menu} />}
       {/* // 3. 하단영역 컴포넌트 */}
       <FooterArea />
     </React.Fragment>
