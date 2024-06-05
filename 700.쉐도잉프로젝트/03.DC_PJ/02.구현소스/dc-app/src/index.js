34eba2ac9c414ac95f467cc72f1d78b46b080ad1
ji0 <buti12@naver.com>
2024-06-05 13:12:32 +0900

    ㄴㅁㅇ

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
@@ -5,6 +5,10 @@ import Layout from "./components/layout/Layout";
 import Character from "./components/pages/Character";
 import Main from "./components/pages/Main";
 import Comics from "./components/pages/Comics";
+import Movies from "./components/pages/Movies";
+import Series from "./components/pages/Series";
+import Games from "./components/pages/Games";
+import News from "./components/pages/News";
 
 // 전체공통 css 불러오기
 import "../src/css/index.scss";
@@ -36,6 +40,11 @@ import "../src/css/index.scss";
     2. Layout.jsx 레이아웃 컴포넌트를 루트로 선택
     3. 상단영역 GNB에 <Link to> 셋팅
     4. 메인영역에 <Outlet /> 셋팅
+    5. 라우터 연결흐름은
+      (1) Route의 path정보 세팅
+      (2) Link to 정보 클릭 시 1번 정보와 대조
+      (3) 1번 정보 일치시 element에 등록된 컴포넌트 로딩
+      (4) oulet표시 컴포넌트에 삽입
     
 *********************************************/
 
@@ -51,9 +60,15 @@ export default function MainComponent() {
           {/* 하위 라우트 셋팅 
         -> path설정대신 index키워드를 쓰면 첫페이지로 구성됨
         -> MainArea 컴포넌트 <Outlet/>에 출력된다*/}
+        {/* path에 적힌 이름을 부르면 element에 적힌 컴포넌트가 호출됨 */}
+        {/* path에 적힌 이름은 TopArea.jsx에 적힌 Link to~어쩌구에 맞춤 */}
           <Route index element={<Main />} />
           <Route path="character" element={<Character />} />
           <Route path="comics" element={<Comics />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/series" element={<Series />} />
+          <Route path="games" element={<Games />} />
+          <Route path="news" element={<News />} />
 
         </Route>
         {/* Layout루트 Route로 하위 Route를 감싼다 */}
