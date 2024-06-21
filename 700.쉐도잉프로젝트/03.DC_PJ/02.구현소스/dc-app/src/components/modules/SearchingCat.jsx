3399898c986bfe2a7598a2e306b8d007f0b43f8f
ji0 <buti12@naver.com>
2024-06-21 12:53:37 +0900

    ㅇㅇㅇㅇ

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SearchingCat.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SearchingCat.jsx
@@ -1,12 +1,18 @@
+// DC PJ 캐릭터 검색결과 리스트 컴포넌트
 import React from "react";
 
-// css
+// CSS불러오기
 import "../../css/searching_cat.scss";
+
+// 라우터돔 Link
 import { Link } from "react-router-dom";
 
-function SearchingCat({ dt, total }) {
-  //dt - 검색된 배열데이터
+function SearchingCat({ dt }) {
+  // dt - 검색된 배열데이터
+
   // total - 검색된 배열데이터 개수
+  const total = dt.length;
+  console.log("데이터수:",total);
 
   return (
     <>
@@ -18,11 +24,11 @@ function SearchingCat({ dt, total }) {
               <li key={i}>
                 <Link
                   to="/detail"
-                  // state로 3가지 값을 넘겨줌
+                  /* state로 3가지 값을 넘겨준다! */
                   state={{
-                    cname: v.cname,
-                    cdesc: v.cdesc,
-                    facts: v.facts,
+                    cname: v.cname, // 캐릭터이름
+                    cdesc: v.cdesc, // 캐릭터설명
+                    facts: v.facts, // 캐릭터상세
                   }}
                 >
                   <img src={v.tmsrc} alt={v.cname} />
@@ -34,7 +40,7 @@ function SearchingCat({ dt, total }) {
         )
       }
       {
-        // 선택데이터가 0개
+        // 선택데이터가 0개이면 아래 출력
         total == 0 && (
           <h2 style={{ textAlign: "center" }}>
             Sorry, we don't have any matches for that. But there's plenty more
