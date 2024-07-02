8cca29124a12ac168c153a0cecfc1540112358e3
ji0 <buti12@naver.com>
2024-07-02 15:17:46 +0900

    로그인- 전역변수 사용

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
@@ -1,19 +1,56 @@
-// 레이아웃영역 컴포넌트
+// 전체 레이아웃 컴포넌트 ///
 
+import { useState } from "react";
 import FooterArea from "./FooterArea";
 import MainArea from "./MainArea";
 import TopArea from "./TopArea";
 
+// 컨텍스트 API 불러오기
+import { dCon } from "../modules/dCon";
+import { useNavigate } from "react-router-dom";
+
 export default function Layout() {
+  // [ 상태관리 변수 ] //////////////
+  // 1. 로그인 상태관리변수
+  const [loginSts, setLoginSts] = useState(sessionStorage.getItem("minfo"));
+  // -> 초기값으로 세션스토리지 "minfo"를 할당함
+
+  // 2. 로그인 환영 메시지 상태변수
+  const [loginMsg, setLoginMsg] = useState(null);
+  console.log(loginMsg);
+
+  // [ 공통 함수 ] ///
+  // 1. 라우팅 이동함수
+  const goPage = useNavigate();
+  // 2. 로그인 환영메시지 생성함수
+  const makeMsg = (name) => {
+    // 유저아이콘
+    let usrIcon = ["🙍‍♂️","🧏‍♀️","🦸‍♂","👨‍🎤","🦸‍♀"];
+    // 랜덤수 : 0~4사이의 수
+    let rdm = Math.floor(Math.random()*5);
+    // 로그인 메시지 상태변수 업데이트
+    setLoginMsg(`Welcome ${name} ${usrIcon[rdm]}`);
+
+  };
+
+  //// 코드 리턴구역 //////////////
   return (
-    <>
-      {/* 상단영역 */}
+    // Provider value속성으로 전역노출 변수를 설정함!
+    <dCon.Provider
+      value={{
+        loginSts,
+        setLoginSts,
+        setLoginMsg,
+        goPage,
+        makeMsg,
+      }}
+    >
+      {/* 1.상단영역 */}
       <TopArea />
-      {/* 메인영역 */}
+      {/* 2.메인영역 */}
       <MainArea />
-      {/* 하단영역 */}
+      {/* 3.하단영역 */}
       <FooterArea />
-      
-    </>
+    </dCon.Provider>
   );
-} ///////////////Layout//////////////
+} /////////// Layout /////////////////////
