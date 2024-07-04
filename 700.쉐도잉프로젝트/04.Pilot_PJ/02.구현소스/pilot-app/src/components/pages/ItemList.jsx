5468f382c169eb541d51cb43937a5c3670ad230a
ji0 <buti12@naver.com>
2024-07-04 13:23:20 +0900

    상품 상세

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/pages/ItemList.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/pages/ItemList.jsx
@@ -1,14 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 // 상품데이터 불러오기
 import itemListData from "../../js/data/item_list";
+
+// 공통함수 불러오기
+import { addComma } from "../../js/func/common";
+
 
 // css
 import "../../css/item_list.scss";
 
 import $ from "jquery";
+import ItemDetail from "../modules/ItemDetail";
 
-function ItemList(props) {
+
+function ItemList() {
+  // 상태변수
+  // [1] 카테고리 정보
+  const [cat, setCat] = useState(itemListData[0].cat);
+  // [2] 상품 정보
+  const [ginfo, setGinfo] = useState(itemListData[0].ginfo);
+
+
+
   // 화면랜더링
   useEffect(() => {
     $("html,body").css({ overflow: "visible" });
@@ -27,16 +41,30 @@ function ItemList(props) {
           <input type="checkbox" className="chkbx" id="style" defaultChecked />
         </div>
         <div className="grid">
-          <div>
-            <a href="#">
-              [1]
-              <img src="./images/goods/men/m1.png" alt="dress" />
-              <aside>
-                <h2>[남성]카모전판프린트 PQ 티셔츠</h2>
-                <h3>99,000원</h3>
-              </aside>
-            </a>
-          </div>
+          {itemListData.map((v,i) => (
+            <div key={i}>
+              <a href="#" onClick={(e)=>{
+                e.preventDefault();
+                // 상품상세모듈 전달 상태변수 변경
+                setCat(v.cat);
+                setGinfo(v.ginfo);
+
+                // 상세상품정보박스 보이기
+                $(".bgbx").show();
+
+              }}>
+                [{i+1}]
+                <img
+                  src={process.env.PUBLIC_URL + `/images/goods/${v.cat}/${v.ginfo[0]}.png`}
+                  alt="dress"
+                />
+                <aside>
+                  <h2>{v.ginfo[1]}</h2>
+                  <h3>{addComma(v.ginfo[3])}원</h3>
+                </aside>
+              </a>
+            </div>
+          ))}
         </div>
       </section>
       {/* 상세 상품정보 박스 */}
@@ -51,98 +79,10 @@ function ItemList(props) {
           zIndex: "9999",
         }}
       >
-        <a href="#" className="cbtn">
-          <span className="ir">닫기버튼</span>
-        </a>
-        <div id="imbx">
-          <div className="inx">
-            <section className="gim/g">
-              <img src="./images/goods/men/m1.png" alt="큰 이미지" />
-              <div className="small">
-                <a href="#">
-                  <img src="./images/goods/men/m1.png" alt="썸네일 이미지" />
-                  <img src="./images/goods/men/m2.png" alt="썸네일 이미지" />
-                  <img src="./images/goods/men/m3.png" alt="썸네일 이미지" />
-                  <img src="./images/goods/men/m4.png" alt="썸네일 이미지" />
-                  <img src="./images/goods/men/m5.png" alt="썸네일 이미지" />
-                  <img src="./images/goods/men/m6.png" alt="썸네일 이미지" />
-                </a>
-              </div>
-            </section>
-            <section className="gdesc scbar">
-              <h1>HOME &gt; MEN</h1>
-              <div>
-                <ol>
-                  <li>
-                    <img src="./images/dx_ico_new-28143800.gif" alt="new버튼" />
-                  </li>
-                  <li id="gtit">상품명: [남성]카모전판프린트 PQ 티셔츠</li>
-                  <li>
-                    <img
-                      src="./images/icon_type02_social01.gif"
-                      alt="페이스북"
-                    />
-                    <img src="./images/icon_type02_social02.gif" alt="트위터" />
-                    <img src="./images/icon_mail02.gif" alt="이메일" />
-                    <img src="./images/btn_source_copy.gif" alt="URL복사" />
-                  </li>
-                  <li>
-                    <span>판매가</span>
-                    <span id="gprice">99,000원</span>
-                  </li>
-                  <li>
-                    <span>적립금</span>
-                    <span>
-                      <img src="./images/icon_my_m02.gif" alt="적립금" />
-                      4,950(5%적립)
-                    </span>
-                  </li>
-                  <li>
-                    <span>무이자할부</span>
-                    <span>
-                      부분 무이자 할부 혜택
-                      <img
-                        src="./images/view_btn_nointerest_card.gif"
-                        alt="무이자카드보기"
-                      />
-                    </span>
-                  </li>
-                  <li>
-                    <span>상품코드</span> <span id="gcode">DMTS7K731-MG</span>
-                  </li>
-                  <li>
-                    <span>사이즈</span> <span>95 100 105 110</span>
-                  </li>
-                  <li>
-                    <span>구매수량</span>
-                    <span>
-                      <input type="text" id="sum" defaultValue="1" />
-                      <b className="chg_num">
-                        <img src="./images/cnt_up.png" alt="증가" />
-                        <img src="./images/cnt_down.png" alt="감소" />
-                      </b>
-                    </span>
-                  </li>
-                  <li>
-                    <span>컬러</span> <span></span>
-                  </li>
-                  <li>
-                    <span>권장계절</span> <span>여름</span>
-                  </li>
-                  <li className="tot">
-                    <span>총합계</span>
-                    <span id="total">99,000원</span>
-                  </li>
-                </ol>
-              </div>
-              <div>
-                <button className="btn btn1">BUY NOW</button>
-                <button className="btn">SHOPPING CART</button>
-                <button className="btn">WISH LIST</button>
-              </div>
-            </section>
-          </div>
-        </div>
+
+        {/* 아이템디테일 컴포넌트 불러오기 */}
+        <ItemDetail cat={cat} ginfo={ginfo}/>
+        
       </div>
     </main>
   );
