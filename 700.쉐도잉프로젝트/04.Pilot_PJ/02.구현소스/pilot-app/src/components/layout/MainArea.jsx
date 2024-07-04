8b3645ca45ef1ce858f2c98463a3fd711ecda979
ji0 <buti12@naver.com>
2024-07-04 11:24:46 +0900

    sdsdsddsdssdsdsdsdsdsssdsd

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/MainArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/MainArea.jsx
@@ -1,18 +1,25 @@
-import React, { useMemo, useState } from "react";
+import React from "react";
 import MainCont from "../pages/MainCont";
 import Fashion from "../pages/Fashion";
+import ItemList from "../pages/ItemList";
 
 function MainArea({ page }) {
-  // page = 페이지변경 문자값 전달
-  // 4가지 값 - main / glist / men/ women / style
+  // page 페이지변경 문자값전달
+  // 5가지 값 : main / item-list / men / women / style
+  console.log("메인전달page:",page);
 
+  // 코드 리턴구역 /////////////
   return (
     <>
-      {/* // 조건출력으로 페이지별 분기 */}
+      {/* 조건출력으로 페이지별 분기 */}
       {page == "main" && <MainCont />}
-      {(page == "men" || page == "women" || page == "style") && (
-        <Fashion subCat={page} />
-      )}
+      {page == "item-list" && <ItemList />}
+      {
+      (page == "men" ||
+      page == "women" ||
+      page == "style") 
+      
+      && <Fashion subCat={page} />}
     </>
   );
 }
--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/TotalMenu.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/TotalMenu.jsx
@@ -1,6 +1,6 @@
 // Pilot PJ 전체메뉴 컴포넌트
 
-// 컨택스트 API호출
+// 컨텍스트 API호출
 import { useContext } from "react";
 import { pCon } from "./pCon";
 
@@ -8,85 +8,99 @@ export function TotalMenu() {
   // 컨텍스트 사용하기
   const myCon = useContext(pCon);
 
-  // 서브페이지 이동함수
-  const goSub=(e)=>{
+  // 서브페이지 이동함수 ///
+  const goSub = (e) => {
+    // 1.기본이동막기
     e.preventDefault();
-
-    // 이동할 서브페이지명 : a요소의 글자를 소문자로!
-    let pgName=e.target.innerText.toLowerCase();
-
-    myCon.setMenu(pgName);
-    // 햄버거버튼 클릭이벤트 발생하여 전체메뉴 닫기
+    // 2.이동할 서브 페이지명 : a요소의 글자를 소문자로!
+    let pgName = e.target.innerText.toLowerCase();
+    console.log("이동할 페이지:", pgName);
+    // -> 아이템 리스트 페이지는 중각 공백을 데쉬로 변경
+    pgName = pgName.replace(" ", "-");
+    // 3.서브페이지 이동위해 상태변수업데이트
+    myCon.setPgName(pgName);
+    // 4.햄버거버튼 클릭이벤트 발생하여 전체메뉴 닫기
     document.querySelector(".ham").click();
-  };/////////////////////////goSub////////////////
+  }; ///////// goSub ///////////////
 
-    // 코드 리턴 //////////////////////
-    return (
-      <>
-        <div className="mbox">
-          <video
-            src="./images/disc2018.mp4"
-            loop="loop"
-            muted="muted"
-            className="bgm"
-          ></video>
-          <nav className="mlist">
-            <dl>
-              <dt>
-                <a href="#"
-                  onClick={goSub}
-                >MEN</a>
-              </dt>
-              <dd>
-                <a href="#">T-SHIRT</a>
-              </dd>
-              <dd>
-                <a href="#">JACKET</a>
-              </dd>
-              <dd>
-                <a href="#">TRAINING WARE</a>
-              </dd>
-              <dd>
-                <a href="#">BEACH WARE</a>
-              </dd>
-            </dl>
-            <dl>
-              <dt>
-                <a href="#">WOMEN</a>
-              </dt>
-              <dd>
-                <a href="#">T-SHIRT</a>
-              </dd>
-              <dd>
-                <a href="#">JACKET</a>
-              </dd>
-              <dd>
-                <a href="#">TRAINING WARE</a>
-              </dd>
-              <dd>
-                <a href="#">BEACH WARE</a>
-              </dd>
-            </dl>
-            <dl>
-              <dt>
-                <a href="#">STYLE</a>
-              </dt>
-              <dd>
-                <a href="#">COLLECTION</a>
-              </dd>
-              <dd>
-                <a href="#">SEASON AD</a>
-              </dd>
-              <dd>
-                <a href="#">STAR &amp; NEWS</a>
-              </dd>
-              <dd>
-                <a href="#">MAIN ITEM</a>
-              </dd>
-            </dl>
-          </nav>
-        </div>
-      </>
-    );
-  } ///////// TotalMenu 컴포넌트 //////////
-  
+  // 코드 리턴 //////////////////////
+  return (
+    <>
+      <div className="mbox">
+        <video
+          src="./images/disc2018.mp4"
+          loop="loop"
+          muted="muted"
+          className="bgm"
+        ></video>
+        <nav className="mlist">
+          <dl>
+            <dt>
+              <a href="#" onClick={goSub}>
+                MEN
+              </a>
+            </dt>
+            <dd>
+              <a href="#">T-SHIRT</a>
+            </dd>
+            <dd>
+              <a href="#">JACKET</a>
+            </dd>
+            <dd>
+              <a href="#">TRAINING WARE</a>
+            </dd>
+            <dd>
+              <a href="#">BEACH WARE</a>
+            </dd>
+          </dl>
+          <dl>
+            <dt>
+              <a href="#" onClick={goSub}>
+                WOMEN
+              </a>
+            </dt>
+            <dd>
+              <a href="#">T-SHIRT</a>
+            </dd>
+            <dd>
+              <a href="#">JACKET</a>
+            </dd>
+            <dd>
+              <a href="#">TRAINING WARE</a>
+            </dd>
+            <dd>
+              <a href="#">BEACH WARE</a>
+            </dd>
+          </dl>
+          <dl>
+            <dt>
+              <a href="#" onClick={goSub}>
+                STYLE
+              </a>
+            </dt>
+            <dd>
+              <a href="#">COLLECTION</a>
+            </dd>
+            <dd>
+              <a href="#">SEASON AD</a>
+            </dd>
+            <dd>
+              <a href="#">STAR &amp; NEWS</a>
+            </dd>
+            <dd>
+              <a href="#">MAIN ITEM</a>
+            </dd>
+          </dl>
+          <dl>
+            <dt>
+              <a href="#" onClick={goSub}>
+                Item List
+              </a>
+            </dt>
+            <dd></dd>
+          </dl>
+        </nav>
+      </div>
+    </>
+  );
+} ///////// TotalMenu 컴포넌트 //////////
