ae62542a108a985b355d932f84d1d1247af96b0d
ji0 <buti12@naver.com>
2024-07-05 17:17:18 +0900

    ,,,m

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/ItemDetail.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/modules/ItemDetail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { addComma } from "../../js/func/common";
 
 import $ from "jquery";
@@ -6,6 +6,10 @@ import $ from "jquery";
 function ItemDetail({ cat, ginfo, dt, setGinfo }) {
   // cat - 카테고리,
   // ginfo - 상품 정보
+
+  const getGinfo = useRef(ginfo);
+
+  if (getGinfo.current != ginfo) getGinfo.current = ginfo;
 
   // 배열 생성 테스트
   // 1. 배열변수 = [] -> 배열 리터널
@@ -23,6 +27,51 @@ function ItemDetail({ cat, ginfo, dt, setGinfo }) {
   // console.log(Array(10).fill(7));
   // console.log(Array(10).fill(7,2));
   // console.log(Array(10).fill(7,2,5));
+
+  // 화면 랜더링 구역
+  useEffect(() => {
+    // [ 수량증감 버튼클릭시 증감기능 ]
+
+    // 1. 대상 요소
+    // 숫자출력 input
+    const sum = $("#sum");
+    // 수량증감 이미지 버튼
+    const numBtn = $(".chg_num img");
+    // 총합계 요소
+    const total = $("#total");
+
+    // 2. 수량증감 이벤트 함수
+    numBtn.on("click", (e) => {
+      // 1. 이미지 순번
+      let seq = $(e.target).index();
+      // console.log(seq);
+      // 0은 증가 / 1은 감소
+
+      // 2. 기존 숫자값 읽기
+      let num = Number(sum.val());
+      console.log("현재숫자", num);
+
+      // 3. 증감반영하기
+      sum.val(!seq ? ++num : num == 1 ? 1 : --num);
+      // 증감기호가 변수 앞에 있어야 먼저 증감하고 할당함
+
+      // 문제!!! ginfo값으로 읽으면 최초에 세팅된 값이 그대로 유지된다.
+      // >>> 본 함수는 최초 한번만 세팅되기 떄문에
+      // 해결!! 새로 들어오는 ginfo값을 참조변수에 넣어서 본 함수에서 그 값을 읽으면 된다.
+
+      // 4. 토탈값 계산해서 넣기
+      // 원가격은 참조변수 getGinfo 사용 >> 매번 업데이트 됨
+      total.text(addComma(getGinfo.current[3] * num) + "원");
+    });
+    // numBtn.off("click") <<제거용
+  }, []); ///////////////////////////////////useEffect
+
+  // 화면랜더링구역 : 매번
+  useEffect(()=>{
+    // 매번 리랜더링될때마다 수량,총합계 초기화
+    $("#sum").val(1);
+    $("#total").text(addComma(getGinfo.current[3]) + "원");
+  });
 
   return (
     <>
@@ -55,7 +104,7 @@ function ItemDetail({ cat, ginfo, dt, setGinfo }) {
                 .fill("")
                 .map((v, i) => {
                   // 한줄리스트와 같은번호면 6번 오게함
-                  let num = ginfo[0].substr(1) == i + 1 ? 6: i + 1;
+                  let num = ginfo[0].substr(1) == i + 1 ? 6 : i + 1;
                   // 현재상품번호가 1~5중 같은게 있으면 6번
                   // substr(시작순번,개수)->개수없으면 순번부터 다 가져옴
                   // console.log("검사번호", num);
