2f934e51aa8144966e54cf6fd73f2f72f44bf2e2
ji0 <buti12@naver.com>
2024-07-08 12:20:10 +0900

    게시판 글쓰기,글보기

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
@@ -3,28 +3,42 @@
 // 사용자 기본 정보 생성 함수
 import { initData } from "../func/mem_fn";
 
-// 로컬스토리지 게시판 기본 데이터 JSON
-import baseData from "../data/board.json";
+// 로컬스토리지 게시판 기본 데이터 JSON -> 로컬스로 대체
+// import baseData from "../data/board.json";
 // 리액트 웹펙에서 제이슨은 이름을 지어서 불러오면 된다.
 // 제이슨 파일 처리는 다르므로 확장자는 반드시 씀
 
 // console.log(baseData);
 
 // 제이쿼리
-import $ from "jquery";
+import $, { data } from "jquery";
 
 import "../../css/board.scss";
 import "../../css/board_file.scss";
 import { Fragment, useRef, useState } from "react";
 
+//로컬 스토리지 확인 JS
+import { initBoardData } from "../func/board_fn.js";
+
 export default function Board() {
+  // 로컬스토리지 게시판 데이터 정보확인
+  initBoardData();
+
+  // 로컬스 데이터 변수 할당하기
+  const baseData = JSON.parse(localStorage.getItem("board-data"));
+
   // [ 상태관리 변수 ]
   // 1. 페이지번호
   const [pageNum, setPageNum] = useState(1);
+  // 2. 기능모드
+  const [mode, setMode] = useState("L");
 
   // [ 참조변수 ]
   // 1. 전체 개수 - 매번 계산하지않도록 참조변수로 만듦
   const totalCount = useRef(baseData.length);
+  // 2. 선택데이터 저장
+  const selRecord = useRef(null);
+  // 특정리스트 글 제목 클릭시 데이터 저장
 
   // 2. 페이지당 개수
   const unitSize = 8;
@@ -58,13 +72,21 @@ export default function Board() {
       if (i >= totalCount.current) break;
       selData.push(orgData[i]);
     }
-    console.log("일부데이터", selData);
+    // console.log("일부데이터", selData);
 
     return selData.map((v, i) => (
       <tr key={i}>
         <td>{i + 1 + sNum}</td>
         <td>
-          <a href="#" data-idx="51">
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              setMode("R");
+              // 해당데이터 저징
+              selRecord.current = v;
+            }}
+          >
             {v.cont}
           </a>
         </td>
@@ -118,51 +140,53 @@ export default function Board() {
     return pgCode;
   }; /////////////////pagingList함수
 
+  // 버튼클릭시 변경함수
+  const clickButton=(e)=>{
+    let btnText=e.target.innerText;
+    console.log(btnText);
+
+    switch(btnText){
+      case "Write":console.log("글쓰기");break;
+      case "List":setMode("L");break;
+    }
+  };
+
   //// 코드 리턴구역 //////////////
   return (
     <>
       <main className="cont">
         <h1 className="tit">OPINION</h1>
-        <div className="selbx">
-          <select name="cta" id="cta" className="cta">
-            <option value="tit">Title</option>
-            <option value="cont">Contents</option>
-            <option value="unm">Writer</option>
-          </select>
-          <select name="sel" id="sel" className="sel">
-            <option value="0">Descending</option>
-            <option value="1">Ascending</option>
-          </select>
-          <input id="stxt" type="text" maxLength="50" />
-          <button className="sbtn">Search</button>
-        </div>
-        <table className="dtbl" id="board">
-          <thead>
-            <tr>
-              <th>Number</th>
-              <th>Title</th>
-              <th>Writer</th>
-              <th>Date</th>
-              <th>Hits</th>
-            </tr>
-          </thead>
-          <tbody>{bindList()}</tbody>
-          <tfoot>
-            <tr>
-              <td colSpan="5" className="paging">
-                {pagingList()}
-              </td>
-            </tr>
-          </tfoot>
-        </table>
+        {
+          // 리스트 모드일 경우 리스트 출력하기
+          mode == "L" && (
+            <ListMode bindList={bindList} pagingList={pagingList} />
+          )
+        }
+        {
+          // 읽기 모드일 경우 상세보기 출력하기
+          mode == "R" && <ReadMode selRecord={selRecord} />
+        }
         <br />
         <table className="dtbl btngrp">
           <tbody>
             <tr>
               <td>
-                <button>
-                  <a href="#">Write</a>
-                </button>
+                {
+                  // 글씨기 버튼은 로그인 상태이고 리스트모드일때만
+                  mode == "L" && (
+                    <button onClick={clickButton}>
+                      Write
+                    </button>
+                  )
+                }
+                {
+                  // 읽기 상태일 경우
+                  mode == "R" && (
+                    <button onClick={clickButton}>
+                      List
+                    </button>
+                  )
+                }
               </td>
             </tr>
           </tbody>
@@ -171,3 +195,97 @@ export default function Board() {
     </>
   );
 } /////////// Board /////////////////////
+
+/*************************************************
+ * 리스트 모드 서브 컴포넌트
+ *************************************************/
+const ListMode = ({ bindList, pagingList }) => {
+  return (
+    <>
+      <div className="selbx">
+        <select name="cta" id="cta" className="cta">
+          <option value="tit">Title</option>
+          <option value="cont">Contents</option>
+          <option value="unm">Writer</option>
+        </select>
+        <select name="sel" id="sel" className="sel">
+          <option value="0">Descending</option>
+          <option value="1">Ascending</option>
+        </select>
+        <input id="stxt" type="text" maxLength="50" />
+        <button className="sbtn">Search</button>
+      </div>
+      <table className="dtbl" id="board">
+        <thead>
+          <tr>
+            <th>Number</th>
+            <th>Title</th>
+            <th>Writer</th>
+            <th>Date</th>
+            <th>Hits</th>
+          </tr>
+        </thead>
+        <tbody>{bindList()}</tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="5" className="paging">
+              {pagingList()}
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </>
+  );
+}; ////////////////////////////////ListMode
+
+/*************************************************
+ * 읽기 모드 서브 컴포넌트
+ *************************************************/
+const ReadMode = ({ selRecord }) => {
+  // 읽기 모드가 호출되었다는 것은 리스트의 제목이 클릭되었다는 것을 의미
+  // 따라서 현재 레코드값도 저장되었다는 의미
+  console.log("전달된 참조변수", selRecord.current);
+  return (
+    <>
+      <table className="dtblview readone">
+        <caption>OPINION : Read</caption>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>
+              <input type="text" className="name" size="20" readOnly />
+            </td>
+          </tr>
+          <tr>
+            <td>Title</td>
+            <td>
+              <input
+                type="text"
+                className="subject"
+                size="60"
+                readOnly
+                value={data.num}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>Content</td>
+            <td>
+              <textarea
+                className="content"
+                cols="60"
+                rows="10"
+                readOnly
+                value={data.cont}
+              ></textarea>
+            </td>
+          </tr>
+          <tr>
+            <td>Attachment</td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}; ////////////////////////////////ReadMode
