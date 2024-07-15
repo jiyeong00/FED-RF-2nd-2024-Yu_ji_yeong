47bca2e87cb8a5b0af7c2342dbc3009180c76724
ji0 <buti12@naver.com>
2024-07-15 16:24:51 +0900

    ㄴㄴㄴ

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
@@ -1,5 +1,5 @@
 // 오피니언 페이지 컴포넌트 ///
-import { Fragment, useContext, useRef, useState } from "react";
+import { Fragment, useContext, useEffect, useRef, useState } from "react";
 
 // 사용자 기본정보 생성 함수
 import { initData } from "../func/mem_fn";
@@ -32,6 +32,11 @@ export default function Board() {
 
   // 로컬스 데이터 변수할당하기!
   const baseData = JSON.parse(localStorage.getItem("board-data"));
+
+  // 원본 데이터에 정렬 적용하기 : 내림차순
+  baseData.sort((a, b) =>
+    Number(a.idx) > Number(b.idx) ? -1 : Number(a.idx) < Number(b.idx) ? 1 : 0
+  );
 
   // [ 상태관리 변수 ] ///
   // [1] 페이지 번호
@@ -200,8 +205,46 @@ export default function Board() {
       case "Modify":
         setMode("M");
         break;
+      // 삭제일 경우 삭제함수 호출
+      case "Delete":
+        deleteFn();
+        break;
     }
   }; ////////// clickButton //////////
+
+  // 삭제 처리함수 //////////////
+  const deleteFn = () => {
+    // 삭제여부확인
+    if (window.confirm("Are you sure you want to delete?")) {
+      // 1. 해당항목 idx담기
+      let currIdx = selRecord.current.idx;
+      // 2. some()로 순회하여 해당항목 삭제하기
+      // find()와 달리 some()은 결과값을 boolean값으로
+      // 리턴하여 처리한다! 이것을 이용하여 코드처리해보자!
+      baseData.some((v, i) => {
+        if (v.idx == currIdx) {
+          // 해당순번 배열값을 삭제하자!
+          // 배열삭제는  splice(순번,1)
+          baseData.splice(i, 1);
+
+          // 리턴true할 경우 종료!
+          return true;
+        } ///// if ////
+      }); ///// some ///////
+
+      // 3. 로컬스에 업데이트하기 //////
+      localStorage.setItem("board-data", JSON.stringify(baseData));
+
+      // 4. 삭제후 리스트 리랜더링시 리스트 불일치로 인한
+      // 에러를 방지하기 위하여 전체 개수를 바로 업데이트한다!
+      totalCount.current = baseData.length;
+
+
+      // 4. 리스트로 돌아가기 -> 리랜더링 /////
+      // -> 모드변경! "L"
+      setMode("L");
+    } ///////// if ///////////////
+  }; //////// deleteFn ///////////////
 
   // 서브밋 처리함수 //////////////
   const submitFn = () => {
@@ -264,12 +307,69 @@ export default function Board() {
       // 로컬스 확인!
       // console.log(localStorage.getItem("board-data"));
 
-      // 5. 리스트로 돌아가기 /////
+      // 4. 추가후 리스트 리랜더링시 리스트 불일치로 인한
+      // 에러를 방지하기 위하여 전체 개수를 바로 업데이트한다!
+      totalCount.current = baseData.length;
+
+      // 5. 리스트로 돌아가기 -> 리랜더링 /////
       // -> 모드변경! "L"
       setMode("L");
     } /// if ///
+
     // 3. 수정모드 서브밋 (mode=="M")
+    else if (mode == "M") {
+      // 1. 오늘날짜 생성하기 /////
+      // -> 수정시 수정날짜 항목을 새로 만들고 입력함!
+      let today = new Date();
+      // yy-mm-dd 형식으로 구하기
+      // 제이슨 날짜형식 : toJSON()
+      // ISO 표준형식 : toISOString()
+      // 시간까지 나오므로 앞에 10자리만 가져감!
+      // 문자열.substr(0,10)
+
+      // 2. 현재 데이터 idx값
+      let currIdx = selRecord.current.idx;
+
+      // 3. 기존 데이터로 찾아서 변경하기
+      // : 로컬스 데이터 -> baseData
+      // find()는 특정항목을 찾아서 리턴하여 데이터를 가져
+      // 오기도 하지만 업데이트 등 작업도 가능함!
+      baseData.find((v) => {
+        // console.log(v,selRecord);
+        if (v.idx == currIdx) {
+          // [ 업데이트 작업하기 ]
+          // 기존항목변경 : tit, cont
+          // 이미 선택된 selRecord 참조변수의 글번호인 idx로
+          // 원본 데이터를 조회하여 기존 데이터를 업데이트함!
+
+          // (1) 글제목 : tit
+          v.tit = title;
+          // (2) 글내용 : cont
+          v.cont = cont;
+          // 추가항목
+          // (원래는 확정된 DB스키마에 따라 입력해야하지만
+          // 우리가 사용하는 로컬스토리지의 확장성에 따라 필요한
+          // 항목을 추가하여 넣는다!)
+          // (3) 수정일 : mdate
+          v.mdate = today.toJSON().substr(0, 10);
+
+          // 해당항목을 만나면 끝남!
+          return true;
+        } /// if ///
+      }); /////// find 메서드 /////////
+
+      // 4. 로컬스에 업데이트하기 //////
+      localStorage.setItem("board-data", JSON.stringify(baseData));
+
+      // 로컬스 확인!
+      // console.log(localStorage.getItem("board-data"));
+
+      // 5. 리스트로 돌아가기 /////
+      // -> 모드변경! "L"
+      setMode("L");
+    }
   }; ////////// submitFn //////////////
+
 
   //// 코드 리턴구역 //////////////
   return (
@@ -307,25 +407,27 @@ export default function Board() {
               {
                 // 2. 읽기상태 "R" 일 경우
                 <>
-                {mode == "R" && 
-                <button onClick={clickButton}>List</button>}
+                  {mode == "R" && <button onClick={clickButton}>List</button>}
 
-                {
-                  // console.log("비교:",
-                  // JSON.parse(sts).uid,
-                  // "==?",
-                  // selRecord.current.uid)
-                }
-                
-                { // 로그인한 상태이고 글쓴이와 일치할때
-                // 수정보드 이동버튼이 노출됨!
-                // 현재글은 selRecord 참조변수에 저장됨
-                // 글정보 항목중 uid 가 사용자 아이디임!
-                // 로그인 상태정보하위의 sts.uid와 비교함
-                (mode == "R" && sts && 
-                JSON.parse(sts).uid==selRecord.current.uid) && 
-                <button onClick={clickButton}>Modify</button>
-                }
+                  {
+                    // console.log("비교:",
+                    // JSON.parse(sts).uid,
+                    // "==?",
+                    // selRecord.current.uid)
+                  }
+
+                  {
+                    // 로그인한 상태이고 글쓴이와 일치할때
+                    // 수정보드 이동버튼이 노출됨!
+                    // 현재글은 selRecord 참조변수에 저장됨
+                    // 글정보 항목중 uid 가 사용자 아이디임!
+                    // 로그인 상태정보하위의 sts.uid와 비교함
+                    mode == "R" &&
+                      sts &&
+                      JSON.parse(sts).uid == selRecord.current.uid && (
+                        <button onClick={clickButton}>Modify</button>
+                      )
+                  }
                 </>
               }
               {
