a29df041e6df9db8fc4557d16feda3ab92567501
ji0 <buti12@naver.com>
2024-07-18 12:20:08 +0900

    싱크맞추기 + 게시판 조회수 증가

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
@@ -97,7 +97,9 @@ export default function Board() {
       selData.push(orgData[i]);
     } ///// for //////
 
-    // console.log("일부데이터:", selData);
+    console.log("일부데이터:", selData);
+    console.log("여기:", selData.length);
+    if (selData.length == 0) setPageNum(pageNum - 1);
 
     return selData.map((v, i) => (
       <tr key={i}>
@@ -239,10 +241,11 @@ export default function Board() {
       // 에러를 방지하기 위하여 전체 개수를 바로 업데이트한다!
       totalCount.current = baseData.length;
 
-
       // 4. 리스트로 돌아가기 -> 리랜더링 /////
       // -> 모드변경! "L"
       setMode("L");
+      // -> 삭제후 첫페이지로 이동!
+      setPageNum(1);
     } ///////// if ///////////////
   }; //////// deleteFn ///////////////
 
@@ -309,11 +312,14 @@ export default function Board() {
 
       // 4. 추가후 리스트 리랜더링시 리스트 불일치로 인한
       // 에러를 방지하기 위하여 전체 개수를 바로 업데이트한다!
-      totalCount.current = baseData.length;
+      // 이때 실제로 업데이트된 locals 배열객체의 개수를 센다!
+      totalCount.current = locals.length;
 
       // 5. 리스트로 돌아가기 -> 리랜더링 /////
       // -> 모드변경! "L"
       setMode("L");
+      // -> 추가후 첫페이지로 이동!
+      setPageNum(1);
     } /// if ///
 
     // 3. 수정모드 서브밋 (mode=="M")
@@ -370,7 +376,6 @@ export default function Board() {
     }
   }; ////////// submitFn //////////////
 
-
   //// 코드 리턴구역 //////////////
   return (
     <main className="cont">
@@ -381,7 +386,7 @@ export default function Board() {
       }
       {
         // 2. 읽기 모드일 경우 상세보기 출력하기
-        mode == "R" && <ReadMode selRecord={selRecord} />
+        mode == "R" && <ReadMode selRecord={selRecord} sts={sts} />
       }
       {
         // 3. 쓰기 모드일 경우 로그인 정보 보내기
@@ -502,7 +507,9 @@ const ListMode = ({ bindList, pagingList }) => {
 /****************************************** 
         읽기 모드 서브 컴포넌트
 ******************************************/
-const ReadMode = ({ selRecord }) => {
+const ReadMode = ({ selRecord, sts }) => {
+  // selRecord - 현재글정보 / sts - 로그인 사용자정보
+
   // 읽기 모드가 호출되었다는 것은
   // 리스트의 제목이 클릭되었다는 것을 의미!
   // 따라서 현재 레코드 값도 저장되었다는 의미!
@@ -510,6 +517,67 @@ const ReadMode = ({ selRecord }) => {
   // 전달된 데이터 객체를 변수에 할당
   const data = selRecord.current;
 
+  // [ 조회수 증가하기 ]
+  // 규칙1 : 자신의 글은 증가하지 않는다!
+  // 규칙2 : 타인의 글은 증가한다!
+  // 규칙3 : 로그인한 상태에서 한번만 증가한다!
+
+  // ((조회된 글 저장방법))
+  // -> 세션스토리지는 적합! 창을 닫으면 사라지니까!
+  // -> 쿠키는 삭제방법이 즉각적이지 못하므로 제외!
+  // -> 참조변수는 새로고침하면 초기화 되므로 제외!
+
+  // 1.없으면 세션스 만들기
+  if (!sessionStorage.getItem("bd-rec")) {
+    sessionStorage.setItem("bd-rec", "[]");
+  }
+  // 2.(1) 세션스에 글번호 저장하기
+  let rec = JSON.parse(sessionStorage.getItem("bd-rec"));
+
+  // (2)기존 배열값에 현재글번호 존재여부 검사하기
+  // 결과가 true이면 조회수를 증가하지 않는다.
+  let isRec = rec.includes(data.idx);
+
+  // (3) 로그인한 사용자의 글이면 isRec값을 true처리하기
+  // sts가 true면 즉, 로그인 한 사용자이면 처리
+  if (sts) {
+    console.log(
+      "선택글 아이디: ",
+      data.uid,
+      "로그인 사용자 아이디:",
+      JSON.parse(sts).uid
+    );
+    // 글쓴이 아이디와 로그인사용자 아이디가 같은가?
+    if (data.uid == JSON.parse(sts).uid) {
+      // 글번호저장과 조회수 증가를 하지 않도록 함
+      isRec = true;
+    } ///if
+  }
+
+  // (4)배열에 값 추가하기 : 기존값에 업으면 넣기
+  if (!isRec) rec.push(data.idx);
+  // (5)다시 세션스에 저장하기
+  sessionStorage.setItem("bd-rec", JSON.stringify(rec));
+
+  // 3. 글번호 증가하기
+  // 게시판 원본 데이터에 조회수 업데이트하기
+  // (1) 게시판 로컬스데이터 파싱
+  let bdData = JSON.parse(localStorage.getItem("board-data"));
+  // (2) 게시판 해당 데이터 cnt값 증가
+  // 조건:isRec값이 false일때
+  if (!isRec) {
+    bdData.some((v) => {
+      if (v.idx == data.idx) {
+        // 기존값에 1증가하여 넣기
+        v.cnt = Number(v.cnt) + 1;
+        return true;
+      } ////////if
+    }); /////////some
+    // (3) 다시 로컬스에 저장하기
+    localStorage.setItem("board-data", JSON.stringify(bdData));
+  } ///////if///////////
+
+  /////// 코드리턴 구역 ///////////
   return (
     <>
       <table className="dtblview readone">
