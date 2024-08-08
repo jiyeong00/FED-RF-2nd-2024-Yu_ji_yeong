5ded1ee01bd991da729caa72991c196ad9a53d3d
ji0 <buti12@naver.com>
2024-08-08 14:12:06 +0900

    vercel넣엇따.....이거 어캐하느,ㄴ데

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
@@ -122,8 +122,17 @@ export default function Board() {
         }
         // 리턴코드값은 리듀서 변수에 할당!
         return (
-          
-          gval + (gval != "" ? "*" : "") + txt
+          // 숙제: *문자열이 있으면 split으로 잘라서
+          // 배열로 만들고 배열값중 현재 입력된 txt가
+          // 배열중에 없으면 새로 등록하고 있으면
+          // 등록하지 않는다를 코드로 작성할것!
+          // 힌트1: 등록않는다는 gval만 넣으면 됨
+          // 힌트2: 배열값 중 단순비교는 includes()사용!
+          gval.indexOf("*")!==-1
+          ? gval.split("*").includes(txt)
+          ? gval
+          : gval + (gval != "" ? "*" : "") + txt
+          : gval + (gval != "" ? "*" : "") + txt
         );
       }
       // (2) 전체리스트 돌아기기 실행코드
@@ -148,7 +157,7 @@ export default function Board() {
         // (3) 기존 키워드 재검색일 경우 실행코드
       case "again": {
         // 검색기준값 읽어오기
-        let creteria = $(ele).siblings(".cta").val();
+        let creteria = $("#cta").val();
         console.log("기준값:", creteria);
         // 검색어 읽어오기
         let txt = $(ele).text();
@@ -170,7 +179,20 @@ export default function Board() {
           alert("Please enter a keyword!");
         }
         // 리턴코드값은 리듀서 변수에 할당!
-        return gval + (gval != "" ? "*" : "") + txt;
+        // 리턴코드값은 리듀서 변수에 할당!
+        return (
+          // 숙제: *문자열이 있으면 split으로 잘라서
+          // 배열로 만들고 배열값중 현재 입력된 txt가
+          // 배열중에 없으면 새로 등록하고 있으면
+          // 등록하지 않는다를 코드로 작성할것!
+          // 힌트1: 등록않는다는 gval만 넣으면 됨
+          // 힌트2: 배열값 중 단순비교는 includes()사용!
+          gval.indexOf("*")!==-1
+          ? gval.split("*").includes(txt)
+          ? gval
+          : gval + (gval != "" ? "*" : "") + txt
+          : gval + (gval != "" ? "*" : "") + txt
+        );
       }
     }
   };
